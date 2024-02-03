4b47cb2c08e69f6adef298b45418b3d48299fd50
Satish2021N <saishdevilliers17@gmail.com>
2024-02-03 13:45:23 +0545

    Auth Middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
-    //Decoding the token
+    //Decoding the token 
+    //Token Validation
     const token = req.headers.authorization.split(" ")[1];
 
     if (!token) {
